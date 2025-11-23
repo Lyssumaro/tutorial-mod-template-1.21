f11644561d70930c7f0338a1ef196c6bbf37eab9
Paul <paulhoerdt07@gmail.com>
2025-11-23 13:42:54 +0100

    Initial commit

---
+++ src/main/java/net/paul/tutorialmod/TutorialMod.java
@@ -0,0 +1,26 @@
+package net.paul.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.paul.tutorialmod.block.ModBlocks;
+import net.paul.tutorialmod.item.ModItemGroups;
+import net.paul.tutorialmod.item.ModItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// Very important comment
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+
+		FuelRegistry.INSTANCE.add(ModItems.STARLIGHT_ASHES, 600);
+	}
+}
