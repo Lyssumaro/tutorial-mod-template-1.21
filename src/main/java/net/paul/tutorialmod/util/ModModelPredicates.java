389df8b2a6824aba7e0aedab3a3522d3da811587
Paul <paulhoerdt07@gmail.com>
2025-12-06 14:29:35 +0100

    Custom Bow | #22

--- src/main/java/net/paul/tutorialmod/util/ModModelPredicates.java
+++ src/main/java/net/paul/tutorialmod/util/ModModelPredicates.java
@@ -1,6 +1,7 @@
 package net.paul.tutorialmod.util;
 
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
+import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.paul.tutorialmod.TutorialMod;
 import net.paul.tutorialmod.component.ModDataComponentTypes;
@@ -11,5 +12,21 @@ public class ModModelPredicates {
         ModelPredicateProviderRegistry.register(ModItems.CHISEL, Identifier.of(TutorialMod.MOD_ID, "used"),
                 (stack, world, entity, seed) -> stack.get(ModDataComponentTypes.COORDINATES) != null ? 1f : 0f);
 
+        registerCustomBow(ModItems.KAUPEN_BOW);
+    }
+
+    private static void registerCustomBow(Item item) {
+        ModelPredicateProviderRegistry.register(item, Identifier.ofVanilla("pull"), (stack, world, entity, seed) -> {
+            if (entity == null) {
+                return 0.0F;
+            } else {
+                return entity.getActiveItem() != stack ? 0.0F : (float)(stack.getMaxUseTime(entity) - entity.getItemUseTimeLeft()) / 20.0F;
+            }
+        });
+        ModelPredicateProviderRegistry.register(
+                item,
+                Identifier.ofVanilla("pulling"),
+                (stack, world, entity, seed) -> entity != null && entity.isUsingItem() && entity.getActiveItem() == stack ? 1.0F : 0.0F
+        );
     }
 }
